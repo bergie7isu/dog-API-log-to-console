37e2c785f345537fa893af92e685930a4d24401a
Mark Bergstrom <markbergstrom@gmail.com>
2019-08-11 21:31:14 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function logResults(jsonResponse) {
+    for (i = 0; i < jsonResponse.message.length; i++) {
+        console.log(jsonResponse.message[i]);
+    }
+}
+
+function getDogPictures(numberOfDogs) {
+    let fetchUrl = "https://dog.ceo/api/breeds/image/random/" + numberOfDogs;
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(jsonResponse => logResults(jsonResponse));
+}
+
+function howManyPictures() {
+    let numberOfDogs = $('#number-of-dogs').val();
+    getDogPictures(numberOfDogs);
+}
+function watchForSubmit() {
+    $('form').submit(function(event) {
+        event.preventDefault();
+        howManyPictures();
+    })
+}
+
+$(watchForSubmit)
